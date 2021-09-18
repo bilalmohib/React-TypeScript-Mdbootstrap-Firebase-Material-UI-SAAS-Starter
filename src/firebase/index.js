8e46bcf71eeef02108b315cbb13feb3d4ea8e3cf
Muhammad Bilal Mohib-ul-Nabi <bilalmohib7896@gmail.com>
2021-09-18 20:54:09 +0500

    Update index.js

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -6,14 +6,14 @@ import 'firebase/database'
 
 try {
     const firebaseConfig = {
-        apiKey: process.env.REACT_APP_API_KEY,
-        authDomain: process.env.REACT_APP_authDomain,
-        databaseURL: process.env.REACT_APP_databaseURL,
-        projectId:  process.env.REACT_APP_projectId,
-        storageBucket: process.env.REACT_APP_storageBucket,
-        messagingSenderId: process.env.REACT_APP_messagingSenderId,
-        appId: process.env.REACT_APP_appId,
-        measurementId:  process.env.REACT_APP_measurementId
+        apiKey: 'process.env.REACT_APP_API_KEY',
+        authDomain: 'process.env.REACT_APP_authDomain',
+        databaseURL: 'process.env.REACT_APP_databaseURL',
+        projectId:  'process.env.REACT_APP_projectId',
+        storageBucket: 'process.env.REACT_APP_storageBucket',
+        messagingSenderId: 'process.env.REACT_APP_messagingSenderId',
+        appId: 'process.env.REACT_APP_appId',
+        measurementId:'process.env.REACT_APP_measurementId'
     };
     // Initialize Firebase    
     firebase.initializeApp(firebaseConfig);
